8c42fdd2e00250b87074a2431a73b5f73eb2d777
Colby Watts <wattsc4365@student.faytechcc.edu>
2025-09-21 00:09:45 +0000

    Errors  fixed

--- M1LAB1_090x.cpp
+++ M1LAB1_090x.cpp
@@ -9,7 +9,19 @@ Colby Watts
 #include <iostream>
 using namespace std;
 
-int maihn{} (
+int main() {
+// This program acts like a storefront
+// Set up our variables
+string item_name = "shirts";
+int number_of_items = 15;
+double cost_each = 11.99; // $11.99
 
+// Greet the customer
+cout << "welcome to the " << item_name << " Shop!" << endl;
 
-)
+// Assk for the order
+
+// Calculate the total price
+
+// Out put the results
+}
