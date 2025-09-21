a47e48add743da5ba3f2817f0c6b2fea1925a74d
Colby Watts <wattsc4365@student.faytechcc.edu>
2025-09-21 01:22:24 +0000

    M2t1 ; fixed price still?

--- M2tLab 090x.cpp
+++ M2tLab 090x.cpp
@@ -5,6 +5,7 @@
 // we're going to make the simple possible
 // "checkout" machine.
 #include <iostream>
+#include <iomanip> // for the 2 decimal places
 using namespace std;
 
 
@@ -15,7 +16,7 @@ string first_name, last_name, full_name; // holds customer name
 string product = "shirts";
 int amount_purchase; 
 double cost_each = 14.99;
-double total_cost;
+double total_cost = amount_purchase * cost_each;
 
 
 // Greet the customer
@@ -26,16 +27,21 @@ cout << "What's your last name? ";
 cin >> last_name;
 full_name = first_name + " " + last_name;
 cout << "Nice to meet you, " << full_name << endl;
-cin >> amount_purchase;
-
 
 // Ask how much they'd like to purchase
 cout << "How many " << product << " would you like today?";
-
+cin >> amount_purchase;
 
 // Calculate total price
-// Give the result
+total_cost = amount_purchase = cost_each;
 
+// formatting: set all prices to 2 decimal places
+cout <<  setprecision(2) << fixed;
+
+// Give the result
+cout << "For " << amount_purchase << " " << product << endl;
+cout << "That will be $" << total_cost << endl;
+cout << "Thank you for your purchase, have a nice day!" << endl;
 
 
 return 0; // no errors
