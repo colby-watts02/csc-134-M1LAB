aa2a16f6968ed3c1799f5cf2ecbaa1e17ec6d800
Colby Watts <wattsc4365@student.faytechcc.edu>
2025-09-21 00:40:24 +0000

    start M2t1

---
+++ M2tLab 090x.cpp
@@ -0,0 +1,27 @@
+// CSC 134
+// M2t1 - product sales
+// Colby Watts
+//9/20/25
+// we're going to make the simple possible
+// "checkout" machine.
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+// Set up all variables
+
+
+
+
+
+
+// Greet the customer
+
+
+
+
+
+
+}
