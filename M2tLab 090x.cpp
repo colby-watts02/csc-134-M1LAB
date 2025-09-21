2a09d5f232857e6b024a835d52815941da90cc4f
Colby Watts <wattsc4365@student.faytechcc.edu>
2025-09-21 00:47:39 +0000

    layout finsished

--- M2tLab 090x.cpp
+++ M2tLab 090x.cpp
@@ -11,17 +11,24 @@ using namespace std;
 int main() {
 
 // Set up all variables
-
-
-
-
+string first_name, last_name, full_name; // holds customer name
+string product = "shirts";
+double cost_each = 14.99;
+double total_cost;
 
 
 // Greet the customer
+cout << "Welcome to our " << product << " shop!" << endl;
 
 
 
+// Ask how much they'd like to purchase
+
+// Calculate total price
+// Give the result
 
 
+
+return 0; // no errors
 
 }
