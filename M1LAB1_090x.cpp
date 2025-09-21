52c5b9c9450a79a1474eb9a9241198e1f6ee8ecb
Colby Watts <wattsc4365@student.faytechcc.edu>
2025-09-21 00:20:10 +0000

    Finisshed M1lab

--- M1LAB1_090x.cpp
+++ M1LAB1_090x.cpp
@@ -15,13 +15,23 @@ int main() {
 string item_name = "shirts";
 int number_of_items = 15;
 double cost_each = 11.99; // $11.99
+double total_cost;
 
 // Greet the customer
 cout << "welcome to the " << item_name << " Shop!" << endl;
 
 // Assk for the order
+// assume they'll buy everything
+cout << "We have  " << number_of_items << " " << item_name << endl;
+cout << "Each costs $" << cost_each << endl;
 
 // Calculate the total price
+total_cost = number_of_items * cost_each;
 
 // Out put the results
+cout << "to purchase all " << number_of_items << " will cost $" << total_cost << " total." << endl;
+cout << "Thank you, please come again!" << endl; 
+
+return 0; // no errors
+
 }
