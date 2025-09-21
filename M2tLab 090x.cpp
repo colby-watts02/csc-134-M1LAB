3e6111b4d40c02a9c83f96726b99767960184ef6
Colby Watts <wattsc4365@student.faytechcc.edu>
2025-09-21 01:04:23 +0000

    M2t1 ; fixed spacing error

--- M2tLab 090x.cpp
+++ M2tLab 090x.cpp
@@ -13,6 +13,7 @@ int main() {
 // Set up all variables
 string first_name, last_name, full_name; // holds customer name
 string product = "shirts";
+int amount_purchase; 
 double cost_each = 14.99;
 double total_cost;
 
@@ -24,11 +25,13 @@ cin >> first_name;
 cout << "What's your last name? ";
 cin >> last_name;
 full_name = first_name + " " + last_name;
-cout << "Nice to meet you, " << first_name << last_name << endl;
-
+cout << "Nice to meet you, " << full_name << endl;
+cin >> amount_purchase;
 
 
 // Ask how much they'd like to purchase
+cout << "How many " << product << " would you like today?";
+
 
 // Calculate total price
 // Give the result
